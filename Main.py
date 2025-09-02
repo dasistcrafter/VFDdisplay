4c0328fbd8a174fa5416439ab67f3ecdb216d8a9
Sakier <dasistcrafter@gmail.com>
2025-09-02 20:43:39 +0200

    vu mether was working

--- Main.py
+++ Main.py
@@ -1,31 +1,13 @@
-from threading import Thread, Event
-from tkinter import *
-from volume1 import volume1Static
-from getdataVU import *
-
-event = Event
-
-#ad menue to select what is shown
-
-"""
-TODO:
-thred calculate levvels
-
-"""
+from threading import Thread
+from volume1 import start_gui
+from getdataVU import _calculate_levels
 
 LevelL = [0]
 LevelR = [0]
 
+def start():
+    Thread(target=_calculate_levels, args=(LevelL, LevelR), daemon=True).start()
+    start_gui(LevelL, LevelR)
+
 if __name__ == '__main__':
-    Thread(target=volume1Static).start()
-    Thread(target=volume1aktive, args=(LevelL, LevelR)).start()
-    Thread(target=calculate_levels, args=(LevelL, LevelR)).start()
-
-#volume/VU
-volume1Static()
-volume1aktive(LevelL,LevelR)
-
-#VU data
-calculate_levels(LevelL, LevelR)
-
-print(f"L:{LevelL} R:{LevelR}")
+    start()
--- volume1.py
+++ volume1.py
@@ -1,31 +1,47 @@
 from tkinter import *
 
 root = Tk()
-C = Canvas(root, bg="gray55",height=200,width=1000)
+C = Canvas(root, bg="gray55", height=200, width=1000)
+C.pack()
 
-def create_circle(x, y, r, o, w, C): #center coordinates, radius, outline Color, width
+barL = None
+barR = None
+
+def create_circle(x, y, r, o, w, C):
     x0 = x - r
     y0 = y - r
     x1 = x + r
     y1 = y + r
-    return C.create_oval(x0, y0, x1, y1,outline= o, width= w )
+    return C.create_oval(x0, y0, x1, y1, outline=o, width=w)
 
 def volume1Static():
-   frame = C.create_rectangle(10,10,990,190,outline ="black",fill ="gray15",width = 2)
-   volumecontrollring = create_circle(900,85,40, "black", 6, C)
-   displaybg = C.create_rectangle(50,30,800,140, fill='#4a1500')
-   displayoutline =  C.create_rectangle(50,30,800,140,outline ='black', width = 4)
-   displayVUr = C.create_text(80, 76, text="L", fill="lightblue", font=("Arial", 16))
-   displayVUl = C.create_text(80, 112, text="R", fill="lightblue", font=("Arial", 16))
+    C.create_rectangle(10, 10, 990, 190, outline="black", fill="gray15", width=2)
+    create_circle(900, 85, 40, "black", 6, C)
+    C.create_rectangle(50, 30, 800, 140, fill='#4a1500')
+    C.create_rectangle(50, 30, 800, 140, outline='black', width=4)
+    C.create_text(80, 76, text="L", fill="lightblue", font=("Arial", 16))
+    C.create_text(80, 112, text="R", fill="lightblue", font=("Arial", 16))
 
-def volume1aktive(LevelL,LevelR):
-   outburstL = LevelL * 5
-   outburstLvisualL = C.create_rectangle(100,68,700,84, fill='lightblue')
+def volume1aktive(LevelL, LevelR):
+    global barL, barR
+    C.delete("bar")
 
-   
+    widthL = min(LevelL[0] * 10, 700)
+    widthR = min(LevelR[0] * 10, 700)
+
+    for i in 10:
+        if 100+widthL => i * 70
+    
+    barL = C.create_rectangle(100, 68, 100 + widthL, 84, fill='lightblue', tags="bar")
+    barR = C.create_rectangle(100, 104, 100 + widthR, 120, fill='lightblue', tags="bar")
 
 
-volume1Static()
-volume1aktive()
-C.pack()
-mainloop()
+    
+def update_gui(LevelL, LevelR):
+    volume1aktive(LevelL, LevelR)
+    root.after(100, update_gui, LevelL, LevelR)
+
+def start_gui(LevelL, LevelR):
+    volume1Static()
+    update_gui(LevelL, LevelR)
+    root.mainloop()
