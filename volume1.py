1b0a6a1d0ca36de1846f33addc45e876bc2a7c7a
Sakier <dasistcrafter@gmail.com>
2025-09-02 21:43:08 +0200

    addet block desing in volum1aktive

--- volume1.py
+++ volume1.py
@@ -21,25 +21,50 @@ def volume1Static():
     C.create_rectangle(50, 30, 800, 140, outline='black', width=4)
     C.create_text(80, 76, text="L", fill="lightblue", font=("Arial", 16))
     C.create_text(80, 112, text="R", fill="lightblue", font=("Arial", 16))
+    for i in range(20):#change with num_blocks to match volume1aktive if wanted
+        if i > 12: #change so that it maches with the red blocks
+            C.create_text(114+i*32, 57, text=i, fill="red", font=("Arial", 16)) #chage i*block_width+spacing to match volume1aktive if wanted
+        else:
+            C.create_text(114+i*32, 57, text=i, fill="lightblue", font=("Arial", 16)) #chage i*block_width+spacing to match volume1aktive if wanted
+
+
 
 def volume1aktive(LevelL, LevelR):
     global barL, barR
     C.delete("bar")
 
-    widthL = min(LevelL[0] * 10, 700)
-    widthR = min(LevelR[0] * 10, 700)
-
-    for i in 10:
-        if 100+widthL => i * 70
+    num_blocks = 20
+    block_width = 26
+    spacing = 6
     
-    barL = C.create_rectangle(100, 68, 100 + widthL, 84, fill='lightblue', tags="bar")
-    barR = C.create_rectangle(100, 104, 100 + widthR, 120, fill='lightblue', tags="bar")
+
+    blocksL = int(min(LevelL[0] * 0.34, num_blocks))
+    blocksR = int(min(LevelR[0] * 0.34, num_blocks))
 
 
-    
+    #Left Channel (L)
+    for i in range(blocksL):
+        x0 = 100 + i * (block_width + spacing)
+        x1 = x0 + block_width
+        if x1 > 620:
+            colorL = "red"
+        else:
+            colorL = "lightblue"
+        C.create_rectangle(x0, 68, x1, 84, fill=colorL, tags="bar")
+
+    #Right Channel (R)
+    for i in range(blocksR):
+        x0 = 100 + i * (block_width + spacing)
+        x1 = x0 + block_width
+        if x1 > 620:
+            colorR = "red"
+        else:
+            colorR = "lightblue"
+        C.create_rectangle(x0, 104, x1, 120, fill=colorR, tags="bar")
+
 def update_gui(LevelL, LevelR):
     volume1aktive(LevelL, LevelR)
-    root.after(100, update_gui, LevelL, LevelR)
+    root.after(140, update_gui, LevelL, LevelR)
 
 def start_gui(LevelL, LevelR):
     volume1Static()
